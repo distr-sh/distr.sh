11236b2bba1123cea14aa8a264af098c83cee7d0
Philip Miglinci <pmig@glasskube.com>
2026-02-06 12:32:35 +0100

    chore: small styling stuff
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- src/data/products.ts
+++ src/data/products.ts
@@ -11,49 +11,49 @@ export const products: Product[] = [
     description:
       'Standardize your Docker or Helm deployments embedded with agents in Kubernetes, Docker Compose, VM or Bare Metal environments',
     value: 'rocket',
-    href: '/docs/deployment-agents/',
+    href: '/docs/product/agents/',
   },
   {
     title: 'Container Registry',
     description:
       'Distribute Docker Images, Helm Charts, or every OCI compatible artifact like large LLM Models with our high performance OCI registry',
     value: 'package',
-    href: '/docs/container-registry/',
+    href: '/docs/product/registry/',
   },
   {
     title: 'License Management',
     description:
       'Time based access control and entitlements for Applications and Artifacts - managed in a central place',
     value: 'key',
-    href: '/docs/license-management/',
+    href: '/docs/product/license-management/',
   },
   {
     title: 'Alerts',
     description:
       'Receive notification in realtime when deployments report an error or go stale',
     value: 'bell',
-    href: '/docs/alerts/',
+    href: '/docs/product/agents/',
   },
   {
     title: 'Logs and Metrics',
     description:
       'Collect Logs and Metrics in realtime and directly download them as bundle for better Customer support',
     value: 'chart-line',
-    href: '/docs/logs-metrics/',
+    href: '/docs/product/agents/',
   },
   {
     title: 'Compatibility Matrix',
     description:
       'Automatically test, if your Application is compatible across a matrix of possible deployment environments',
     value: 'check-circle',
-    href: '/docs/compatibility-checks/',
+    href: '/docs/product/agents/',
   },
   {
     title: 'Pre Flight Checks',
     description:
       'Determine if needed resources are available or execute custom pre and post installation scripts',
     value: 'clipboard-check',
-    href: '/docs/pre-flight-checks/',
+    href: '/docs/product/agents/',
   },
   {
     title: 'Customer Portal',
@@ -67,13 +67,13 @@ export const products: Product[] = [
     description:
       'Extend your pull based deployment model with push based approaches with our API Integrations and GitHub Action',
     value: 'workflow',
-    href: '/docs/integrations/',
+    href: '/docs/guides/automatic-deployments-from-github/',
   },
   {
     title: 'Air-gapped',
     description:
       'Distribute your application with air-gapped bundles into the most isolated environments',
     value: 'server-off',
-    href: '/docs/use-cases/air-gapped-deployments/',
+    href: '/docs/use-cases/air-gapped/',
   },
 ];
